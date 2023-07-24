5ca57cb65f468ef8f5e2b63627c8c6a5d1c66667
RAskarov <raskarov@mtsbank.ru>
2023-07-24 19:19:56 +0300

    NODE-1: Рефакторинг команды current

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 #!/usr/bin/env node
 
 const yargs = require('yargs')
-const {hideBin} = require('yargs/helpers')
 
-/**
- * Вывод даты в формате ISO при написании команды current
- */
-const dateIso = new Date().toISOString()
-const argv = yargs(hideBin(process.argv))
-.boolean('current')
-.default('current',() => {
-  console.log(dateIso)
-})
-.argv
+yargs.command({
+  command: 'current',
+  describe: 'return date in ISO format',
+  handler: function () {
+    const dateIso = new Date().toISOString()
+    console.log(dateIso);
+  }
+});
 
+
+yargs.parse();
