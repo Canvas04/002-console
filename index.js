a20acd815dd1d5e3de76ed4b3c676a0489d10990
RAskarov <raskarov@mtsbank.ru>
2023-07-26 15:20:57 +0300

    NODE-1: Сделал команду current

--- index.js
+++ index.js
@@ -5,11 +5,47 @@ const yargs = require('yargs')
 yargs.command({
   command: 'current',
   describe: 'return date in ISO format',
-  handler: function () {
-    const dateIso = new Date().toISOString()
-    console.log(dateIso);
+  handler: function (argv) {
+    const date = new Date()
+    if(argv.year) {
+        console.log(date.getFullYear())
+    }else if (argv.month) {
+        console.log(date.getMonth())
+    }else if(argv.date) {
+        console.log(date.getDate())
+    }else{
+        console.log(date.toISOString())
+    }
   }
-});
+})
+.alias('y','year')
+.alias('m','month')
+.alias('d','date')
+
+// yargs.command({
+//     command: 'add',
+//     describe: 'return date in ISO format',
+//     handler: function (argv) {
+//       const date = new Date()
+//         const a = date.setDate(date.getDate() + 2)   
+//         console.log(date)   
+ 
+//         if(argv.d){
+//             console.log(1)
+//         }
+//     }
+//   })
+//   .alias('d')
+
+//   yargs.command({
+//     command: 'sub',
+//     describe: 'return date in ISO format',
+//     handler: function (argv) {
+//       const date = new Date()
+      
+//     }
+//   })
+//   .alias('m')
 
 
 yargs.parse();
