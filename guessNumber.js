3d584adecf80d7c3e47edac091b69a22b6f23764
RAskarov <raskarov@mtsbank.ru>
2023-07-29 14:24:18 +0300

    NODE-1: Написал программу для загадывания чисел

---
+++ guessNumber.js
@@ -0,0 +1,22 @@
+const readline = require("node:readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Загадано число в диапазоне от 0 до 100", () => {
+  const guessedNumber = 76;
+
+  rl.on("line", (line) => {
+    const userInput = Number(line);
+    if (userInput > guessedNumber) {
+      console.log("Больше");
+    } else if (userInput < guessedNumber) {
+      console.log("Меньше");
+    } else if (userInput === guessedNumber) {
+      console.log(`Отгадано число ${guessedNumber}`);
+      rl.close();
+    }
+  });
+});
