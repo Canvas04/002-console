493ab6f27ebeaa512bffbe552a1fea2381f6626c
RAskarov <raskarov@mtsbank.ru>
2023-08-01 18:35:39 +0300

    NODE-2:Написал интерфейс загаданвания случайного числа пользователю

--- coinFlip.js
+++ coinFlip.js
@@ -1,2 +1,27 @@
 #!/usr/bin/env node
 
+const readline = require("node:readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question(
+  "Игра загадывает случайное число (1 или 2). Попробуйте отгадать",
+  () => {
+    let random = Math.floor(Math.random() * 2);
+    let resultNumber = random + 1;
+
+    rl.on("line", (line) => {
+      const userInput = Number(line);
+
+      if(userInput !== resultNumber) {
+        console.log('Попробуйте еще раз')
+      }else if(userInput === resultNumber) {
+         console.log('Число отгадано')
+          rl.close()
+      }
+    });
+  }
+);
