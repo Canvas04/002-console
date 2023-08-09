cf763f2dc75a9d5b16a750dd1d6f07805c14ef8e
RAskarov <raskarov@mtsbank.ru>
2023-08-10 00:32:05 +0300

    NODE-2: Готова первая версия программа

--- coinFlip.js
+++ coinFlip.js
@@ -1,12 +1,17 @@
 #!/usr/bin/env node
 
 const readline = require("node:readline");
+const path = require("path");
+const fs = require("fs");
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
+//  fs.writeFile(gameResults, String(userInput), (err) => {
+//         if (err) throw Error(err);
+//       });
 rl.question(
   "Игра загадывает случайное число (1 или 2). Попробуйте отгадать",
   () => {
@@ -16,11 +21,14 @@ rl.question(
     rl.on("line", (line) => {
       const userInput = Number(line);
 
-      if(userInput !== resultNumber) {
-        console.log('Попробуйте еще раз')
-      }else if(userInput === resultNumber) {
-         console.log('Число отгадано')
-          rl.close()
+      if (userInput !== resultNumber) {
+        console.log("Попробуйте еще раз");
+      } else if (userInput === resultNumber) {
+       
+        fs.writeFile(process.argv[2],String(userInput),err => {
+            if (err) throw Error(err)
+        })
+        rl.close();
       }
     });
   }
