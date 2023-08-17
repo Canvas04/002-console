0f39dc2630df03f44a711bacd1b302164738868a
RAskarov <raskarov@mtsbank.ru>
2023-08-17 14:15:53 +0300

    NODE-3 Подправил скрипт , чтобы тот мог читать значения

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 //В качестве параметров принимается название города для которого нужно вывести прогноз
 const http = require("http");
 
-const {MY_API_KEY} = require('./config')
+const {MY_API_KEY} = require('./config.js')
 
 const url = `https://api.weatherstack.com/forecast
 ? access_key = ${MY_API_KEY}
 & query = New York`;
 
-console.log(MY_API_KEY)
+console.log(url)
 
 // const server = http.createServer((req, res) => {}).listen(3000);
