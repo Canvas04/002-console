da73c461de88bfff475c209ed357dbd23b796627
RAskarov <raskarov@mtsbank.ru>
2023-08-17 14:30:02 +0300

    NODE-3: Начал делать сервер , подключил nodemon

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 //Нужно написать вывод погоды
 //В качестве параметров принимается название города для которого нужно вывести прогноз
 const http = require("http");
+const url = require("url");
 
-const {MY_API_KEY} = require('./config.js')
+const { MY_API_KEY } = require("./config.js");
 
-const url = `https://api.weatherstack.com/forecast
+const urlForRequest = `https://api.weatherstack.com/forecast
 ? access_key = ${MY_API_KEY}
 & query = New York`;
 
-console.log(url)
+const layoutStart = `
+  <link
+    rel="stylesheet" 
+    href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" 
+    integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" 
+    crossorigin="anonymous"
+  />
+    <div class="container pt-5">
+`;
 
-// const server = http.createServer((req, res) => {}).listen(3000);
+const server = http
+  .createServer((req, res) => {
+    const urlParsed = url.parse(req.url, true);
+    const { pathname, query } = urlParsed;
+    const { method } = req;
+
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+
+    if(pathname === '/') {
+        res.write(layoutStart)
+        res.write(`<h3>Вывод погоды по определенному городу</h3>`)
+    }
+  })
+  .listen(3023);
