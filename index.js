f6c9b6abc65560401ecdbfb35b0fee831257ff1d
RAskarov <raskarov@mtsbank.ru>
2023-08-23 17:16:31 +0300

    library: Создал класс Book

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const { v4: uuid } = require("uuid");
+
+const { PORT } = require("./config.js");
+
+class Book {
+  constructor(
+    id = uuid(),
+    title = "",
+    description = "",
+    authors = "",
+    favorite = "",
+    fileCover = "",
+    fileName = ""
+  ) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.authors = authors;
+    this.favorite = favorite;
+    this.fileCover = fileCover;
+    this.fileName = fileName;
+  }
+}
+
+const stor = {
+  books: [],
+};
+
+const app = express();
+app.use(express.json);
+
+app.get('/', (req, res) => {
+    const {todo} = stor
+    res.write('ehhehe')
+    res.json(todo)
+})
+
+app.listen(PORT);
